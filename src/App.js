5cdae5044d476a706835de4384e06a1da6d876ab
Souvik P50 <souvikp50@gmail.com>
2019-12-31 14:20:43 +0530

    this is try1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h2>This is app.js</h2>
+    <div className='App'>
+      <h2>This is app js</h2>
     </div>
   );
 }
